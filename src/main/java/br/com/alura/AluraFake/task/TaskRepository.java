18ecf8efa3eb577f2aa22edc74ace8bfb8c5af62
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-25 21:58:36 -0300

    refactor(Q-1): adiciona ordenacao por order_item no metodo de busca de tasks por course_id

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,9 +1,12 @@
 package br.com.alura.AluraFake.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findAllByCourseId(Long courseId);
+    @Query("SELECT t FROM Task t WHERE t.course.id = :courseId ORDER BY t.order ASC")
+    List<Task> findTasksByCourseIdOrderByOrderItemAsc(@Param("courseId") Long courseId);
 }
