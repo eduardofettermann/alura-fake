0e52c719e1c2555dbbc71206b1c6d1bbe8578da1
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-26 17:53:54 -0300

    feat(Q-1): adiciona metodo que retorna se existe uma Task com o courseId e statement passados

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -9,4 +9,9 @@ import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     @Query("SELECT t FROM Task t WHERE t.course.id = :courseId ORDER BY t.order ASC")
     List<Task> findTasksByCourseIdOrderByOrderItemAsc(@Param("courseId") Long courseId);
+
+    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END " +
+            "FROM Task t " +
+            "WHERE t.course.id = :courseId AND t.statement = :statement")
+    boolean existsTasksByCourseIdAndByStatement(@Param("courseId") Long courseId, @Param("statement") String statement);
 }
