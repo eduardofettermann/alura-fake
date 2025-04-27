d4090d04edf24e6f180098336768abfc9e799653
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-27 02:46:46 -0300

    feat(Q-1): adiciona metodos que retornam se existe task com courseId e order passados e que procuram as tasks com a order passada ou maior para posteior reordenação

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,13 +1,17 @@
 package br.com.alura.AluraFake.task;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    @Query("SELECT t FROM Task t WHERE t.course.id = :courseId ORDER BY t.order ASC")
+    @Query("SELECT t FROM Task t " +
+            "WHERE t.course.id = :courseId " +
+            "ORDER BY t.order ASC")
     List<Task> findTasksByCourseIdOrderByOrderItemAsc(@Param("courseId") Long courseId);
 
     @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END " +
@@ -15,8 +19,21 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             "WHERE t.course.id = :courseId AND t.statement = :statement")
     boolean existsTasksByCourseIdAndByStatement(@Param("courseId") Long courseId, @Param("statement") String statement);
 
-    @Query("SELECT MAX(t.order)" +
-            "FROM Task t " +
+    @Query("SELECT MAX(t.order) FROM Task t " +
             "WHERE t.course.id = :courseId")
     Integer findMaxOrderByCourseId(@Param("courseId") Long courseId);
+
+    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END " +
+            "FROM Task t " +
+            "WHERE t.course.id = :courseId " +
+            "AND t.order = :order")
+    boolean existsTasksByCourseIdAndByOrder(@Param("courseId") Long courseId, @Param("order") Integer order);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM Task t " +
+            "WHERE t.course.id = :courseId " +
+            "AND t.order >= :order " +
+            "ORDER BY t.order ASC")
+    List<Task> findByCourseIdAndOrderGreaterThanEqualForUpdate(Long courseId, Integer order);
+
 }
