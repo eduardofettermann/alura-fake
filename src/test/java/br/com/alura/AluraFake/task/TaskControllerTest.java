8abec5e097f4b4d5dc82a7f8e0c6d961a8eb400c
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-27 21:56:21 -0300

    test(Q-1): adiciona caso de teste de criação de task do tipo SINGLE_CHOICE com mais de uma alternativa correta

--- src/test/java/br/com/alura/AluraFake/task/TaskControllerTest.java
+++ src/test/java/br/com/alura/AluraFake/task/TaskControllerTest.java
@@ -425,4 +425,35 @@ public class TaskControllerTest {
                 .andExpect(jsonPath("$[0].message")
                         .isNotEmpty());
     }
+
+    @Test
+    void newSingleChoice__should_return_bad_request_when_task_has_more_than_one_correct_alternative() throws Exception {
+        Course course = mock(Course.class);
+        NewSingleChoiceTaskDTO newSingleChoiceTaskDTO = new NewSingleChoiceTaskDTO();
+        newSingleChoiceTaskDTO.setCourseId(course.getId());
+        newSingleChoiceTaskDTO.setType(Type.SINGLE_CHOICE);
+        newSingleChoiceTaskDTO.setOrder(1);
+        newSingleChoiceTaskDTO.setStatement("O que aprendemos hoje?");
+
+        NewAlternativeDTO apiRest = new NewAlternativeDTO();
+        NewAlternativeDTO bestPractices = new NewAlternativeDTO();
+
+        apiRest.setOption("API REST");
+        bestPractices.setOption("Boas práticas");
+
+        apiRest.setCorrect();
+        bestPractices.setCorrect();
+
+        doReturn(Optional.of(course)).when(courseRepository).findById(newSingleChoiceTaskDTO.getCourseId());
+        doReturn(true).when(course).isBuilding();
+
+        mockMvc.perform(post("/task/new/singlechoice")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newSingleChoiceTaskDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].field")
+                        .value("options"))
+                .andExpect(jsonPath("$[0].message")
+                        .isNotEmpty());
+    }
 }
