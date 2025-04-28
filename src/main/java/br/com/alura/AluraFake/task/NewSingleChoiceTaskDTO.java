c431ee8e2858853b9d71eb8d4e61459de2fff7cf
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-27 21:16:35 -0300

    feat(Q-1): adiciona DTO de criação de task do tipo SINGLE_CHOICE

---
+++ src/main/java/br/com/alura/AluraFake/task/NewSingleChoiceTaskDTO.java
@@ -0,0 +1,23 @@
+package br.com.alura.AluraFake.task;
+
+import br.com.alura.AluraFake.alternative.NewAlternativeDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public class NewSingleChoiceTaskDTO extends NewTaskDTO {
+    @NotNull
+    @Size(min = 2, max = 5, message = "The list must have between 2 and 5 alternatives")
+    @Valid
+    private List<NewAlternativeDTO> options;
+
+    public List<NewAlternativeDTO> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<NewAlternativeDTO> options) {
+        this.options = options;
+    }
+}
