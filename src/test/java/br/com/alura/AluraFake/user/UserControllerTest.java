6166d763c874eb6f6e6714be625edfeb23897a72
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-29 09:30:04 -0300

    test(Q-3): atualiza testes de UserController e cria classe de testes da UserService

--- src/test/java/br/com/alura/AluraFake/user/UserControllerTest.java
+++ src/test/java/br/com/alura/AluraFake/user/UserControllerTest.java
@@ -1,8 +1,9 @@
 package br.com.alura.AluraFake.user;
 
+import br.com.alura.AluraFake.exception.domain.DuplicateUserEmailException;
 import br.com.alura.AluraFake.user.dto.NewUserDTO;
+import br.com.alura.AluraFake.user.dto.UserListItemDTO;
 import br.com.alura.AluraFake.user.model.Role;
-import br.com.alura.AluraFake.user.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,41 +12,43 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
+import java.util.List;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
-class UserControllerTest {
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
     void newUser__should_return_bad_request_when_email_is_blank() throws Exception {
-        NewUserDTO newUserDTO = new NewUserDTO("Caio Bugorin", "", Role.STUDENT, null);
+        NewUserDTO newUserDTO = new NewUserDTO("John Doe", "john.doe@example.com", Role.STUDENT, null);
 
-        mockMvc.perform(post("/user/new")
+        mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newUserDTO)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].field").value("email"))
-                .andExpect(jsonPath("$[0].message").isNotEmpty());
+                .andExpect(status().isCreated());
+
+        verify(userService, times(1)).newStudent(newUserDTO);
     }
 
     @Test
     void newUser__should_return_bad_request_when_email_is_invalid() throws Exception {
-        NewUserDTO newUserDTO = new NewUserDTO("Caio Bugorin", "caio", Role.STUDENT, null);
+        NewUserDTO newUserDTO = new NewUserDTO("John Doe", "invalid-email", Role.STUDENT, null);
 
-        mockMvc.perform(post("/user/new")
+        mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newUserDTO)))
                 .andExpect(status().isBadRequest())
@@ -55,41 +58,31 @@ class UserControllerTest {
 
     @Test
     void newUser__should_return_unprocessable_entity_when_email_already_exists() throws Exception {
-        NewUserDTO newUserDTO = new NewUserDTO("Caio Bugorin","caio.bugorin@alura.com.br", Role.STUDENT, null);
+        NewUserDTO newUserDTO = new NewUserDTO("John Doe", "john.doe@example.com", Role.STUDENT, null);
+        doThrow(new DuplicateUserEmailException("email")).when(userService).newStudent(newUserDTO);
 
-        when(userRepository.existsByEmail(newUserDTO.email())).thenReturn(true);
-
-        mockMvc.perform(post("/user/new")
+        mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newUserDTO)))
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(jsonPath("$.field").value("email"))
-                .andExpect(jsonPath("$.message").value("Email de usuário já cadastrado no sistema"));
+                .andExpect(jsonPath("$.message").isNotEmpty());
     }
 
     @Test
-    void newUser__should_return_created_when_user_request_is_valid() throws Exception {
-        NewUserDTO newUserDTO = new NewUserDTO("Caio Bugorin", "caio.bugorin@alura.com.br", Role.STUDENT, null);
+    void listAllUsers__should_return_ok_and_list_users() throws Exception {
+        UserListItemDTO user1 = new UserListItemDTO("John Doe", "john.doe@example.com", Role.STUDENT);
+        UserListItemDTO user2 = new UserListItemDTO("Jane Doe", "jane.doe@example.com", Role.STUDENT);
+        when(userService.listAllUsers()).thenReturn(List.of(user1, user2));
 
-        when(userRepository.existsByEmail(newUserDTO.email())).thenReturn(false);
-
-        mockMvc.perform(post("/user/new")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(newUserDTO)))
-                .andExpect(status().isCreated());
-    }
-
-    @Test
-    void listAllUsers__should_list_all_users() throws Exception {
-        User user1 = new User("User 1", "user1@test.com",Role.STUDENT);
-        User user2 = new User("User 2", "user2@test.com",Role.STUDENT);
-        when(userRepository.findAll()).thenReturn(Arrays.asList(user1, user2));
-
-        mockMvc.perform(get("/user/all")
+        mockMvc.perform(get("/users")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name").value("User 1"))
-                .andExpect(jsonPath("$[1].name").value("User 2"));
-    }
+                .andExpect(jsonPath("$[0].name").value("John Doe"))
+                .andExpect(jsonPath("$[0].email").value("john.doe@example.com"))
+                .andExpect(jsonPath("$[1].name").value("Jane Doe"))
+                .andExpect(jsonPath("$[1].email").value("jane.doe@example.com"));
 
+        verify(userService, times(1)).listAllUsers();
+    }
 }
