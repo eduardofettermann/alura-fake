a0b1b1ff142d266b5c01d757a64e82359c02de62
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-29 09:29:14 -0300

    refactor(Q-3): separa Controller e Service de User

--- src/main/java/br/com/alura/AluraFake/user/UserController.java
+++ src/main/java/br/com/alura/AluraFake/user/UserController.java
@@ -1,40 +1,31 @@
 package br.com.alura.AluraFake.user;
 
-import br.com.alura.AluraFake.exception.domain.DuplicateUserEmailException;
 import br.com.alura.AluraFake.user.dto.NewUserDTO;
 import br.com.alura.AluraFake.user.dto.UserListItemDTO;
-import br.com.alura.AluraFake.user.model.User;
 import jakarta.validation.Valid;
-import org.springframework.http.*;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @Transactional
-    @PostMapping("/user/new")
-    public ResponseEntity newStudent(@RequestBody @Valid NewUserDTO newUser) {
-        if(userRepository.existsByEmail(newUser.email())) {
-            throw new DuplicateUserEmailException("email");
-        }
-
-        User user = newUser.toModel();
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void newUser(@RequestBody @Valid NewUserDTO newUser) {
+        userService.newStudent(newUser);
     }
 
-    @GetMapping("/user/all")
+    @GetMapping
     public List<UserListItemDTO> listAllUsers() {
-        return userRepository.findAll().stream().map(UserListItemDTO::new).toList();
+        return userService.listAllUsers();
     }
-
 }
