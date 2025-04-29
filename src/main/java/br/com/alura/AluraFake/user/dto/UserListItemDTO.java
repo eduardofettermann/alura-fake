8ad1c0d9951e94a1ff8d425c2fe7b7834f3230ba
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-28 22:58:54 -0300

    refactor(Q-3): transforma classe dto de listagem de user para record dto de listagem de user

--- src/main/java/br/com/alura/AluraFake/user/dto/UserListItemDTO.java
+++ src/main/java/br/com/alura/AluraFake/user/dto/UserListItemDTO.java
@@ -5,28 +5,13 @@ import br.com.alura.AluraFake.user.model.User;
 
 import java.io.Serializable;
 
-public class UserListItemDTO implements Serializable {
-
-    private String name;
-    private String email;
-    private Role role;
+public record UserListItemDTO(
+        String name,
+        String email,
+        Role role
+) implements Serializable {
 
     public UserListItemDTO(User user) {
-        this.name = user.getName();
-        this.email = user.getEmail();
-        this.role = user.getRole();
+        this(user.getName(), user.getEmail(), user.getRole());
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
 }
