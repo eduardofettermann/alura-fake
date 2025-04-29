b7428cebe63fe5c618bd7f7312d1c0a8b9c7395d
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-28 23:05:44 -0300

    refactor(Q-3): transforma classe dto de listagem de course para record dto de listagem de course

--- src/main/java/br/com/alura/AluraFake/course/dto/CourseListItemDTO.java
+++ src/main/java/br/com/alura/AluraFake/course/dto/CourseListItemDTO.java
@@ -5,33 +5,19 @@ import br.com.alura.AluraFake.course.model.CourseStatus;
 
 import java.io.Serializable;
 
-public class CourseListItemDTO implements Serializable {
-
-    private Long id;
-    private String title;
-    private String description;
-    private CourseStatus courseStatus;
+public record CourseListItemDTO(
+        Long id,
+        String title,
+        String description,
+        CourseStatus courseStatus
+) implements Serializable {
 
     public CourseListItemDTO(Course course) {
-        this.id = course.getId();
-        this.title = course.getTitle();
-        this.description = course.getDescription();
-        this.courseStatus = course.getStatus();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public CourseStatus getStatus() {
-        return courseStatus;
+        this(
+                course.getId(),
+                course.getTitle(),
+                course.getDescription(),
+                course.getStatus()
+        );
     }
 }
