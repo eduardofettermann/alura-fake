374599af34d9b4efa782800431da1e7b73736aa4
Eduardo Fettermann <eduardo.silva@appmax.com.br>
2025-04-29 08:14:54 -0300

    refactor(Q-3): renomea ValidationExceptionHandler para GlobalExceptionHandler e adiciona tratamento de global de DomainExceptions

--- src/main/java/br/com/alura/AluraFake/util/GlobalExceptionHandler.java
+++ src/main/java/br/com/alura/AluraFake/util/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.alura.AluraFake.util;
 
+import br.com.alura.AluraFake.exception.DomainException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,12 +11,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.List;
 
 @ControllerAdvice
-public class ValidationExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<List<ErrorItemDTO>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<ErrorItemDTO> errors = ex.getBindingResult().getFieldErrors().stream().map(ErrorItemDTO::new).toList();
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(DomainException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<ErrorItemDTO> handleDomainException(DomainException ex) {
+        ErrorItemDTO error = new ErrorItemDTO(ex.getField(), ex.getMessage());
+        return ResponseEntity.unprocessableEntity().body(error);
     }
 }
