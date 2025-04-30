1c9ae80d84389e7b48b5386ea9eab642ad0d2ea1
Eduardo Fettermann <eduardofettermann212@gmail.com>
2025-04-30 00:30:03 -0300

    feat(B-1): adiciona autenticação e autorização para que apenas instrutores publiquem courses e adicionem tasks

---
+++ src/main/java/br/com/alura/AluraFake/authentication/AuthenticationController.java
@@ -0,0 +1,42 @@
+package br.com.alura.AluraFake.authentication;
+
+import br.com.alura.AluraFake.exception.EmailAlreadyRegisteredException;
+import br.com.alura.AluraFake.exception.EmailOrPasswordInvalidException;
+import br.com.alura.AluraFake.user.dto.UserAuthenticationDTO;
+import br.com.alura.AluraFake.user.dto.UserLoginResponseDTO;
+import br.com.alura.AluraFake.user.dto.UserRegisterDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponseDTO> login(@RequestBody @Valid UserAuthenticationDTO userAuthenticationDTO) throws EmailOrPasswordInvalidException {
+        Optional<UserLoginResponseDTO> userLoginResponseDTO = authenticationService.login(userAuthenticationDTO);
+        if (userLoginResponseDTO.isPresent()) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new UserLoginResponseDTO("Invalid credentials"));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody @Valid UserRegisterDTO userRegisterDTO) throws EmailAlreadyRegisteredException {
+        authenticationService.register(userRegisterDTO);
+    }
+}
