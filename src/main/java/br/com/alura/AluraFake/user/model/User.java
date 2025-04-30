607b8d2d660c9b8fe5ee012ff4dad9ae664d9b2b
Eduardo Fettermann <eduardofettermann212@gmail.com>
2025-04-29 23:05:32 -0300

    feat(B-1): adiciona herança da UserDetails

--- src/main/java/br/com/alura/AluraFake/user/model/User.java
+++ src/main/java/br/com/alura/AluraFake/user/model/User.java
@@ -2,11 +2,16 @@ package br.com.alura.AluraFake.user.model;
 
 import br.com.alura.AluraFake.util.PasswordGeneration;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,7 +58,37 @@ public class User {
         return Role.INSTRUCTOR.equals(this.role);
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
